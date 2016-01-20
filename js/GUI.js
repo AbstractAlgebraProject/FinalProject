21e0381a1941f332e889e0103f614b38c9f051d5
Nathan Jewell <nathanielx.jewell@gmail.com>
2016-01-20 00:11:13 -0800

    donee?????

--- js/GUI.js
+++ js/GUI.js
@@ -54,8 +54,9 @@ function addTextField(idName, width, placeholder, inputid, xPosition, yPosition,
     }
     $(idName).append('<div class="input-group" style="width: ' + width + ';" id="' + inputid + '-div"><span class="input-group-btn"><button class="btn btn-default" type="button" id="' + inputid + '-button">Go!</button></span><input type="text" class="form-control" placeholder="' + placeholder + '" id="' + inputid + '-input" aria-describedby="sizing-addon1"></div>');
     var checkY = document.getElementById(inputid + "-div").offsetTop, checkX = document.getElementById(inputid + "-div").offsetLeft;
-    document.getElementById(inputid + "-div").style.marginLeft = xPosition - checkX + "px";
-    document.getElementById(inputid + "-div").style.marginTop = yPosition - checkY + "px";
+	document.getElementById(inputid + "-div").style.position = 'absolute';
+    document.getElementById(inputid + "-div").style.left = xPosition - checkX + "px";
+    document.getElementById(inputid + "-div").style.top = yPosition - checkY + "px";
     $('#' + inputid + "-button").on('click', thisCallback);
 }
 
